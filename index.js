26188b034737941e0421bdd32497994b1e51f4fe
htlWels <franz.reitinger@htl-wels.at>
2022-11-17 14:20:04 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const http = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const server = http.createServer((req, res) => {
   // get the file path from req.url, or '/public/index.html' if req.url is '/'
   const filePath = req.url === '/' ? '/index.html' : req.url;
@@ -16,4 +21,8 @@ const server = http.createServer((req, res) => {
   // pipe the proper file to the res object
   res.writeHead(200, { 'Content-Type': contentType });
   fs.createReadStream(`${__dirname}/images/${filePath}`).pipe(res);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
